62b10b45b3039e6b294102199684118ac5a3a0bd
Miquefil <miquefil@gmail.com>
2024-04-16 15:10:55 -0300

    finish?

--- TDC/top.v
+++ TDC/top.v
@@ -1,21 +1,24 @@
-//`include "defines.v"
-
-
 `define NUM_TAPS    = 120
 `define NUM_DECODE  = 7
+`define COUNTER_DIG = 10
+`define DIG_OUT     = 7+7+10
 
-module top #(parameter NUM = 12)  (
+
+
+
+module top (
     input              iClk,
     input              iRst,
     input              iHit,
-    output[NUM-1:0]    taps,
-    output[NUM-1:0]    ff
+    //output[NUM_TAPS-1:0]    taps,
+    output[DIG_OUT-1:0]     oTDC
     );
 
-    wire                rst;                //manages internal and external rst
-    wire                Rise, Fall;
-    wire[NUM_TAPS]      Stop, Start;
-
+    wire                        rst;                //manages internal and external rst
+    wire                        Rise, Fall;
+    wire[NUM_TAPS-1:0]          Stop, Start;
+    wire[NUM_DECODE-1:0]        DecodedStop, DecodedStart;
+    wire[COUNTER_DIG-1:0]       CoarseStamp;
 
 
     Edge u_EdgeDetector(
@@ -37,5 +40,63 @@ module top #(parameter NUM = 12)  (
         .outTaps           ( ),        //output from the Carrys output
         .outFF             ( ) 
     );
+
+    DecodeStart #(`NUM_TAPS, `NUM_DECODE) u_DecStart(
+        .wDecoStartIn          (Start),
+        .wDecoStartOut  (DecodedStart)
+    );
+
+    DecodeStop  #(`NUM_TAPS, `NUM_DECODE) u_DecStop(
+        .wDecoStartIn          (Stop),
+        .wDecoStartOut   (DecodedStop)
+    );
+
+    Coarse #(`COUNTER_DIG) u_Coarse (
+    .clk            (iClk),
+    .iRst           (rst),       //reset count
+    .iCE            (iHit),        //enable
+    .iStore         (Fall),
+    oCoarse         (CoarseStamp)   
+);
+
+    //Delay post resultado y Merging//////////////////
+    //COMMENTS: Originalmente parece que Machado sólo usa un clock como delay así que DelayCounter tiene 1 bit
+
+
+
+
+    wire                StartDelay, StopDelay, ConvertionFinish;
+    reg                 DelayCounter;
+    reg[DIG_OUT-1:0]    ValorTDC;
+
+    assign StopDelay = DelayCounter;
+
+    (* dont_touch = "TRUE" *)FDCE #(.INIT(1'b0)) enable_delay_dff(
+    .Q          (StartDelay),
+    .C          (Fall),
+    .CE         (1'b1),
+    .CLR        (StopDelay),
+    .D          (1'b1)
+);
+
+    always @(posedge iClk) begin
+        if (StopDelay) begin
+            DelayCounter <= 0;        
+        end
+        elseif(StartDelay) begin
+            DelayCounter <= DelayCounter + 1'b1;
+        end
+    end
+    
+    always @(posedge iClk) begin
+        if(rst) begin
+            ValorTDC <= 0;
+        end
+        elseif(StopDelay) begin
+            ValorTDC <= {CoarseStamp, DecodedStart, DecodedStop};
+        end
+    end
+
+    assign oTDC = ValorTDC;
 
 endmodule
--- TDC/top_tb.v
+++ TDC/top_tb.v
@@ -1,26 +1,28 @@
-`timescale  1ps/100fs
-//`include "defines.v"
+`timescale  1ps/1ps
 
-`define cant 12
+`define NUM_TAPS    = 120
+`define NUM_DECODE  = 7
+`define COUNTER_DIG = 10
+`define DIG_OUT     = 7+7+10
 
 module top_tb(
-        output wire          clk
     );
-    reg                  clk2;   //para generar el clock
-    reg                  hit;
-    wire[`cant-1:0]      outs;
-    wire[`cant-1:0]      outFF;
+    reg                                clk2;
+    reg                                hit;
+    reg['DIG_OUT-1:0]                  res;
 
-    top #(`cant) my_top(
-        .clk(clk),
-        .hit(hit),
-        .taps(outs),
-        .ff(outFF)
-        );
+    top u_top(
+        .iClk(clk2),
+        .iRst(),
+        .iHit(hit),
+        .oTDC(res)
+    );
 
     assign clk = clk2;
+    always #5 clk2 = ~clk2; //
 
-    always #2 clk2 = ~clk2;
+
+
     initial begin        
         clk2 = 1'b1;    //inicializa el reloj
         hit = 1'b0;
